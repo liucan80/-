418873b725991c9258cb77b5a676d2d760496edb
liucan80 <190633933@qq.com>
2018-02-03 19:51:37 +0800

    init

---
+++ 批量读取文件名.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class 批量读取文件名
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(批量读取文件名))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.AllowDrop = True
+        Me.TextBox1.Location = New System.Drawing.Point(23, 107)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(166, 229)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(23, 400)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 21)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "选择"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"pdf", "igs", "sldprt", "slddrw"})
+        Me.ComboBox1.Location = New System.Drawing.Point(23, 68)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 20)
+        Me.ComboBox1.TabIndex = 2
+        Me.ComboBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(20, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 12)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "选择文件类型"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(136, 400)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 21)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "输出"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(23, 342)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 12)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "文件数量："
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(26, 370)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 12)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "当前目录："
+        '
+        '批量读取文件名
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(446, 456)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "批量读取文件名"
+        Me.Text = "批量读取文件名"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ 批量读取文件名.vb
@@ -0,0 +1,207 @@
+﻿'导入命名空间，导入前需添加引用Microsoft excel 14.0 object library(excel 2010)
+Imports Microsoft.Office.Interop.Excel
+Imports SolidWorks.Interop.sldworks
+Imports SolidWorks.Interop.swcommands
+Imports SolidWorks.Interop.swconst
+Imports SolidWorksTools
+Imports SolidWorks.Interop.swpublished
+Imports FolderBrowserDialog
+
+Public Class 批量读取文件名
+
+    '定义变量 
+
+    '文件名
+    Dim StrFilename As String
+    '文件名数组
+    Dim strFolderName As String()
+    '已过滤文件名数组
+    Dim Sfnaf(1) As String
+    '数组循环开始标志
+    Dim i As String = 0
+    'Excel应用程序对象
+    Dim vbexcel As Microsoft.Office.Interop.Excel.Application
+    'Excel工作薄
+    Dim vbworkbook As Workbook
+    'Excel工作表
+    Dim vbworksheet As Worksheet
+
+    Dim FileNames As New List(Of String)
+    Dim br1 As FolderBrowserDialog.FolderBrowserDialog = New FolderBrowserDialog.FolderBrowserDialog()
+   
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.Text = "pdf"
+    End Sub
+    '定义一个过滤器函数，检查文件后缀名
+    Public Function GetFileType(filename As String) As String
+        Dim k
+        Dim result As String
+        k = Split(filename, ".")
+        result = k(UBound(k))
+        Return result
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        i = 0
+        ReDim Sfnaf(i)
+
+        ' br1.ShowDialog(Me)
+        If br1.ShowDialog(Me) = System.Windows.Forms.DialogResult.OK Then
+            '获取目标路径下所有文件路径，保存到数组StrFolderName
+            strFolderName = System.IO.Directory.GetFiles(br1.DirectoryPath)
+            Label3.Text = "当前目录：" & br1.DirectoryPath
+            TextBox1.Text = ""
+            For Each Me.StrFilename In strFolderName
+                If GetFileType(StrFilename) = ComboBox1.Text Or GetFileType(StrFilename) = ComboBox1.Text.ToUpper() Then
+                    'i = 0
+                    ReDim Preserve Sfnaf(i)
+                    Sfnaf(i) = System.IO.Path.GetFileNameWithoutExtension(StrFilename)
+
+                    FileNames.Add(StrFilename)
+                    TextBox1.Text = TextBox1.Text & System.IO.Path.GetFileNameWithoutExtension(StrFilename) & vbNewLine
+                    i = i + 1
+                    'Try
+                    '    vbexcel = GetObject(, "excel.application")
+                    '    vbexcel.Visible = True
+                    'Catch ex As Exception
+                    '    vbexcel = CreateObject("excel.application")
+                    '    vbexcel.Visible = True
+                    'End Try
+
+
+
+                    'With vbexcel
+                    '    If vbexcel.ActiveWorkbook Is Nothing Then
+                    '        vbworkbook = .Workbooks.Add()
+
+                    '    Else : vbworkbook = vbexcel.ActiveWorkbook
+                    '    End If
+                    '    With vbworkbook
+                    '        If vbexcel.ActiveSheet Is Nothing Then
+                    '            vbworksheet = .Worksheets.Add()
+                    '            vbworksheet.Activate()
+                    '        Else : vbworksheet = vbexcel.ActiveSheet
+                    '        End If
+                    '        With vbworksheet
+                    '            .Cells(i, 1) = System.IO.Path.GetFileNameWithoutExtension(StrFilename)
+                    '        End With
+                    '    End With
+                    'End With
+
+                    'i = i + 1
+                End If
+
+            Next
+        End If
+        Label2.Text = "文件数量：" & (i)
+
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            vbexcel = GetObject(, "excel.application")
+
+            vbexcel.Visible = True
+        Catch ex As Exception
+            vbexcel = CreateObject("excel.application")
+            vbexcel.Visible = True
+        End Try
+
+
+
+        With vbexcel
+            If vbexcel.ActiveWorkbook IsNot Nothing Then
+                vbworkbook = .Workbooks.Add()
+
+            Else : vbworkbook = vbexcel.ActiveWorkbook
+
+            End If
+            With vbworkbook
+                If vbexcel.ActiveSheet Is Nothing Then
+                    'vbworksheet = .Worksheets.Add()
+                    vbworksheet = .Worksheets.Add()
+                    vbworksheet.Activate()
+                Else : vbworksheet = vbexcel.ActiveSheet
+                End If
+                With vbworksheet
+                    .Cells(2, 1) = "代号"
+                    .Cells(2, 2) = "名称"
+                    .Cells(2, 3) = "每台数量"
+                    .Cells(2, 4) = "材质"
+                    .Cells(2, 5) = "设备型号"
+                    .Cells(2, 6) = "表面处理"
+                    .Cells(2, 7) = "加工数量"
+                    .Cells(2, 8) = "交货日期"
+                    .Cells(2, 9) = "备注"
+                    .Range("A1:I1").MergeCells = True
+                    .Cells(1, 1) = New System.IO.DirectoryInfo(br1.DirectoryPath).Name
+                    ' FolderBrowserDialog1.SelectedPath.g()
+                    For j = 0 To Sfnaf.Length - 1
+                        .Cells(j + 3, 1) = Sfnaf(j)
+                        If ComboBox1.Text = "sldprt" Then
+                            Dim customnames As ArrayList
+                            customnames = New ArrayList()
+                            customnames.Add("零件名称")
+                            customnames.Add("数量")
+                            customnames.Add("材料")
+
+
+                            Dim res As New ArrayList()
+                            res = GetCustomPropertys(FileNames.Item(j), customnames)
+                            .Cells(j + 3, 2) = res.Item(0)
+                            .Cells(j + 3, 3) = res.Item(1)
+                            .Cells(j + 3, 4) = res.Item(2)
+                        End If
+                    Next j
+                End With
+            End With
+        End With
+        vbworksheet.Cells.Cells.EntireColumn.AutoFit()
+        vbworksheet.Cells.Cells.Cells.HorizontalAlignment = XlHAlign.xlHAlignCenter
+        ' Sfnaf(i) = System.IO.Path.GetFileNameWithoutExtension(StrFilename)
+
+        '保存excel
+        'Dim dirName() As String = FolderBrowserDialog1.SelectedPath.Split('\\')
+        'Dim temp As String
+        'temp = FolderBrowserDialog1.SelectedPath
+        'Dim temp1 As String = Split(temp, "\")(UBound(Split(temp, "\")))
+        vbworkbook.SaveAs(GetSavePath(br1.DirectoryPath))
+
+    End Sub
+    Private Function GetSavePath(ByVal path As String)
+        Dim savepath As String
+        savepath = path + "\" + Split(path, "\")(UBound(Split(path, "\")))
+        Return savepath
+    End Function
+    Private Function GetCustomPropertys(ByVal filename As String, ByVal custompropertynames As ArrayList)
+        Dim swApp As SldWorks
+        Dim swModel As ModelDoc2
+        Dim swModelDocExt As ModelDocExtension
+        Dim swCustProp As CustomPropertyManager
+        'val变量存储 value
+        Dim val As String
+        'valout变量存储evaluated value
+        Dim valout As String
+        Dim custompropertyname As String
+        Dim evaluatedValues As ArrayList
+        swApp = New SldWorks()
+        swApp.Visible = False
+        swModel = swApp.OpenDoc(filename, 1)
+        swModelDocExt = swModel.Extension
+        swCustProp = swModelDocExt.CustomPropertyManager("")
+        evaluatedValues = New ArrayList()
+        For Each custompropertyname In custompropertynames
+            swCustProp.Get2(custompropertyname, val, valout)
+
+            evaluatedValues.Add(valout)
+        Next
+
+        swApp.CloseAllDocuments(True)
+        Return evaluatedValues
+
+    End Function
+
+End Class
